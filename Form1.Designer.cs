0408d6df2c7e2e4c89571277f8cfedce0499bc00
AspeEje33 <72020324+AspeEje33@users.noreply.github.com>
2021-08-31 14:10:55 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace Uppgift1_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbxTal2 = new System.Windows.Forms.TextBox();
+            this.tbxTal1 = new System.Windows.Forms.TextBox();
+            this.tbxSumma = new System.Windows.Forms.TextBox();
+            this.btnKör = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbxTal2
+            // 
+            this.tbxTal2.Location = new System.Drawing.Point(543, 119);
+            this.tbxTal2.Name = "tbxTal2";
+            this.tbxTal2.Size = new System.Drawing.Size(100, 26);
+            this.tbxTal2.TabIndex = 0;
+            // 
+            // tbxTal1
+            // 
+            this.tbxTal1.Location = new System.Drawing.Point(166, 119);
+            this.tbxTal1.Name = "tbxTal1";
+            this.tbxTal1.Size = new System.Drawing.Size(100, 26);
+            this.tbxTal1.TabIndex = 1;
+            // 
+            // tbxSumma
+            // 
+            this.tbxSumma.Location = new System.Drawing.Point(368, 292);
+            this.tbxSumma.Name = "tbxSumma";
+            this.tbxSumma.Size = new System.Drawing.Size(100, 26);
+            this.tbxSumma.TabIndex = 2;
+            // 
+            // btnKör
+            // 
+            this.btnKör.Location = new System.Drawing.Point(368, 199);
+            this.btnKör.Name = "btnKör";
+            this.btnKör.Size = new System.Drawing.Size(75, 35);
+            this.btnKör.TabIndex = 3;
+            this.btnKör.Text = "Kör";
+            this.btnKör.UseVisualStyleBackColor = true;
+            this.btnKör.Click += new System.EventHandler(this.btnKör_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnKör);
+            this.Controls.Add(this.tbxSumma);
+            this.Controls.Add(this.tbxTal1);
+            this.Controls.Add(this.tbxTal2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbxTal2;
+        private System.Windows.Forms.TextBox tbxTal1;
+        private System.Windows.Forms.TextBox tbxSumma;
+        private System.Windows.Forms.Button btnKör;
+    }
+}
+
